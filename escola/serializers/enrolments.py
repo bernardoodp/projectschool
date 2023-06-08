abfe2b01b66af5662586a602128ac1732e5b8c41
Bernardo Pandolfo <bernardo.pandolfo@longview.com.br>
2023-06-08 10:51:06 -0300

    feat: info student and course in enrolment

--- escola/serializers/enrolments.py
+++ escola/serializers/enrolments.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from escola.models import Enrolment
+from escola.serializers import StudentSerializer, CourseSerializer
 
 class EnrolmentSerializer(serializers.ModelSerializer):
+    student = StudentSerializer()
+    course = CourseSerializer()
     class Meta:
         model = Enrolment
-        fields = '__all__'
+        fields = ['id','student', 'course', 'period']
