22f7fbcc30e5daedfb6f799ac9b36d5b98e96a0e
Bernardo Pandolfo <bernardo.pandolfo@longview.com.br>
2023-06-15 16:03:07 -0300

    feat: auth login in api, and urls for token

--- setup/urls.py
+++ setup/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from escola import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_extensions.routers import NestedRouterMixin
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 class NestedDefaultRouter(NestedRouterMixin, DefaultRouter):
     def __init__(self, *args, **kwargs):
@@ -28,5 +29,8 @@ COURSE.register(
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
-    path('api/v1/', include(ROUTER.urls))
+    path('api/v1/', include(ROUTER.urls)),
+    path('tokens/', TokenObtainPairView.as_view()),
+    path('tokens/refresh', TokenRefreshView.as_view()),
+    path('api/v1/auth/', include('rest_framework.urls'))
 ]
