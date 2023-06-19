d26ef0980be241e3df9eb5982d7f2d32c2473e35
Bernardo Pandolfo <bernardo.pandolfo@longview.com.br>
2023-06-19 14:33:24 -0300

    user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email')
+
+
+admin.site.register(CustomUser, UserAdmin)
 # Register your models here.
