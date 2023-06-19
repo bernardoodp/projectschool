515039d444e399cd3c6a70d711e9ad0fe1c91970
Bernardo Pandolfo <bernardo.pandolfo@longview.com.br>
2023-06-19 10:28:54 -0300

    feat: users model

--- setup/settings.py
+++ setup/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'escola',
+    'users',
     'rest_framework',
     
 
@@ -114,6 +115,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
